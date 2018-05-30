ec7fd1769171cbb395747c894fccb2137fe8fa20
Unknown <20kweeda@fpsct.org>
2018-05-30 09:30:47 -0400

    started javafx

--- Code/JavaFXGame.java
+++ Code/JavaFXGame.java
@@ -1,5 +1,3 @@
-
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -18,47 +16,42 @@ import javafx.stage.Stage;
  */
 public class JavaFXGame extends Application
 {
-    // We keep track of the count, and label displaying the count:
-    private int count = 0;
-    private Label myLabel = new Label("0");
+    private Board board;
+    private Dice dice;
+    private Player p1;
+    private Player p2;
+    private boolean hasWon;
+    private int currentTurn;
 
     @Override
     public void start(Stage stage) throws Exception
     {
-        // Create a Button or any control item
-        Button myButton = new Button("Count");
-
-        // Create a new grid pane
-        GridPane pane = new GridPane();
-        pane.setPadding(new Insets(10, 10, 10, 10));
-        pane.setMinSize(300, 300);
-        pane.setVgap(10);
-        pane.setHgap(10);
-
-        //set an action on the button using method reference
-        myButton.setOnAction(this::buttonClick);
-
-        // Add the button and label into the pane
-        pane.add(myLabel, 1, 0);
-        pane.add(myButton, 0, 0);
-
-        // JavaFX must have a Scene (window content) inside a Stage (window)
-        Scene scene = new Scene(pane, 300,100);
-        stage.setTitle("JavaFX Example");
-        stage.setScene(scene);
-
-        // Show the Stage (window)
+        stage.setTitle("Chutes and Ladders: Created by Sushane and Darren ?");//lol emojis dont work maybe try to fix that
+        //sets text in top left 
+        
+        board = new Board();
+        dice = new Dice();
+        
+        //add a "popup" for text input, also need to account for number of players
+        p1 = new Player("Darren");//can add text fields later, https://docs.oracle.com/javafx/2/ui_controls/text-field.htm
+        p2 = new Player("Sushane");
+        
+        GridPane gp = new GridPane();
+        gp.setVgap(10.0);
+        gp.setHgap(10.0);
+        
+        //add board
+        
+        Scene s = new Scene(gp, 700,700);
+        stage.setScene(s);
         stage.show();
+        
+        
     }
 
-    /**
-     * This will be executed when the button is clicked
-     * It increments the count by 1
-     */
-    private void buttonClick(ActionEvent event)
+    public static void main(String args[])
     {
-        // Counts number of button clicks and shows the result on a label
-        count = count + 1;
-        myLabel.setText(Integer.toString(count));
+        launch(args);
     }
-}
+    
+    }
